de3b3e1193d81d93c4a920964071ac19648a67f4
fengjunzi0404 <95013853+fengjunzi0404@users.noreply.github.com>
2022-04-12 08:53:05 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "c045b52c-842f-4594-b2d5-3bfcb2b05efc",
                         "alterId": 0
                     }
                 ],
